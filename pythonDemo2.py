5e059fd3357dcc4c3f89ad2eb6bc4035cbe94d16
huzejun <huzejun1990>
2023-01-06 02:55:25 +0800

    python基础项目创建+注释 2023-1-6 02:55:20

---
+++ pythonDemo2.py
@@ -0,0 +1,5 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/1/5 22:36
+# @Author : huzejun
+# @File : pythonDemo2
+# @Project : python基础
---
+++ testPython.py
@@ -0,0 +1,3 @@
+print("今儿是周四，又是高兴的一天")
+print("你好，世界")
+print("hello world")
