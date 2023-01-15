07031b1ead851aaa987d569131a7fc32a29d8919
huzejun <huzejun1990>
2023-01-15 23:23:25 +0800

    python基础项 异常 2023-1-15 23:23:19

---
+++ 050_dream_爬虫_异常.py
@@ -0,0 +1,19 @@
+# @Author : huzejun
+# @Time : 2023/1/15 23:18
+
+# fp = open('text.txt','r')
+#
+# fp.read()
+#
+# fp.close()
+# 异常的格式
+# try:
+#     可能出现异常的代码
+# except 异常的类型
+#     友好的提示
+
+try:
+    fp = open('text.txt','r')
+    fp.read()
+except FileNotFoundError:
+    print('系统正在升级，请稍后再试...')
